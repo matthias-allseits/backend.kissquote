4f23ad32ffa6f53fd36963473e4529fd8f0854b5
Matthias Allseits <java@allseits.ch>
2021-12-24 16:23:37 +0100

    api: share-controller added

---
+++ src/Controller/Api/ShareController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Share;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class ShareController extends AbstractFOSRestController
+{
+
+    /**
+     * @Rest\Get ("/share", name="list_shares")
+     * @param Request $request
+     * @return View
+     */
+    public function listShares(Request $request): View
+    {
+        $shares = $this->getDoctrine()->getRepository(Share::class)->findAll();
+
+        return View::create($shares, Response::HTTP_CREATED);
+    }
+
+}
