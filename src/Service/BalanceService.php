025ed2baf952457dc577ecdb28eed300aa5ed9e0
Matthias Allseits <java@allseits.ch>
2023-08-22 22:52:31 +0200

    positions without transaction preparations

--- src/Service/BalanceService.php
+++ src/Service/BalanceService.php
@@ -25,7 +25,7 @@ class BalanceService
     public function getBalanceForPosition(Position $position): Balance
     {
         $balance = new Balance($position);
-        if (false === $position->isCash() && count($position->getTransactions()) > 0) {
+        if (false === $position->isCash()) {
             $currencyName = $position->getCurrency()->getName();
             $lastRate = null;
             $performance = [];
